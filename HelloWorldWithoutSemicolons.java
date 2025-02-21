bf00cd883f58022977f871df04c1ea6bbbe8bf5a
mvziehl <mvziehl@mtu.edu>
2025-02-21 15:46:19 -0500

    Update HelloWorldWithoutSemicolons.java
    Removed the word "void".

--- HelloWorldWithoutSemicolons.java
+++ HelloWorldWithoutSemicolons.java
@@ -1,5 +1,5 @@
 public class HelloWorldWithoutSemicolons {
-  public static void main(String[] args) {
+  public static main(String[] args) {
     if (System.out.printf("Hello World.").equals(new Object())){}
   }
 }
