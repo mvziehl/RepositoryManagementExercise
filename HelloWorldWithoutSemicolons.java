df17dca984c3328fb4837d8c16980dc025c2d5e5
mvziehl <mvziehl@mtu.edu>
2025-02-21 15:43:38 -0500

    Update HelloWorldWithoutSemicolons.java
    Replaced the "!" with "."

--- HelloWorldWithoutSemicolons.java
+++ HelloWorldWithoutSemicolons.java
@@ -1,5 +1,5 @@
 public class HelloWorldWithoutSemicolons {
   public static void main(String[] args) {
-    if (System.out.printf("Hello World!").equals(new Object())){}
+    if (System.out.printf("Hello World.").equals(new Object())){}
   }
 }
